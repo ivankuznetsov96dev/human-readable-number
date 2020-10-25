c194575ae968801b9c6fa9bdfde4b957a67c770b
Ivan Kuzniatsou <ivan.kuznetsov.dev@gmail.com>
2020-10-25 13:38:32 +0300

    feat: human-readable-number task compleated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function toReadable (number) {
-  
+  if (number === 0) return 'zero';
+
+  const wordObj ={
+    0: '',
+    1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
+    6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',
+    11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen',
+    16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen', 20: 'twenty',
+    30: 'thirty', 40: 'forty', 50: 'fifty', 60: 'sixty', 70: 'seventy', 80: 'eighty',
+    90: 'ninety', 100: 'hundred'
+  }
+
+
+  if (number > 99) return `${wordObj[Math.floor(number / 100)]} hundred ${convertNumber(number % 100, wordObj)}`.trimRight();
+  else return convertNumber(number, wordObj);
+};
+
+function convertNumber(number, wordObj) {
+  if (number < 10) return wordObj[number];
+  else if (number >= 10 && number < 20) return wordObj[number];
+  else return `${wordObj[Math.floor(number / 10) * 10]} ${wordObj[number % 10]}`.trimRight();
 }
